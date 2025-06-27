8dc954c3db74ab4e539ef82d42b3a480fcd3f093
Regarddeb <unohumphrey260@gmail.com>
2025-06-27 15:11:23 +0800

    Upate import for url builder

--- src/modules/place-finder/place-finder.service.ts
+++ src/modules/place-finder/place-finder.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { instanceToPlain } from 'class-transformer';
 import configuration from 'src/config/env.validation';
-import { buildUrl } from 'src/common/utils/urlBuilder';
+import { buildUrl } from '../../common/utils/urlBuilder';
 import { Places, FindPlaceParams } from './interfaces/places';
 
 const config = configuration();
